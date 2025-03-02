5e286ab3003f0a4c260d2fa3300e2cbae390b115
nn10am <dickdockmansory@gmail.com>
2025-03-02 22:47:11 +0700

    remove hepler hashing function for admin

--- backend/app/utils/generate_hash.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-plain_password = "12345678"
-hashed_password = bcrypt_context.hash(plain_password)
-
-print(hashed_password)
